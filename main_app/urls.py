f77adf9c8579cd244b902efb0aec8ff38ee540f5
jtreeves <jackson.t.reeves@gmail.com>
2021-01-14 16:19:41 -0500

    [profile] Add User model and build into Cat model and pages

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('cats/<int:cat_id>', views.cats_show, name = 'cats_show'),
     path('cats/create/', views.CatCreate.as_view(), name = 'cats_create'),
     path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name = 'cats_update'),
-    path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name = 'cats_delete')
+    path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name = 'cats_delete'),
+    path('user/<username>/', views.profile, name = 'profile'),
 ]
