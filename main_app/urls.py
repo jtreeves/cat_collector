94d26e64bd10fc21c0616b7ab493ed5fbc8c6cd1
jtreeves <jackson.t.reeves@gmail.com>
2021-01-14 15:46:40 -0500

    [cats] Create forms for update and delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('cats/', views.cats_index, name = 'cats_index'),
     path('cats/<int:cat_id>', views.cats_show, name = 'cats_show'),
     path('cats/create/', views.CatCreate.as_view(), name = 'cats_create'),
-    path('cats/<int:cat_id>/update/', views.CatUpdate.as_view(), name = 'cats_update'),
-    path('cats/<int:cat_id>/delete/', views.CatDelete.as_view(), name = 'cats_delete')
+    path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name = 'cats_update'),
+    path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name = 'cats_delete')
 ]
